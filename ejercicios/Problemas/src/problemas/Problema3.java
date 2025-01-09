b8abdd75fb9df7979e31056563d208e2bb46a05b
Paulo Andres <pbustamante@utpl.edu.ec>
2025-01-09 12:39:36 -0500

    problemas 3 y 4

--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -15,7 +17,60 @@ public class Problema3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+
+        String notaCualitativa;
+        String mensaje;
+        double notas1;
+        double notas2;
+        double notas3;
+        double notas4;
+
+        System.out.println("Ingrese la primera nota");
+        notas1 = entrada.nextDouble();
+        System.out.println("Ingrese la segunda nota");
+        notas2 = entrada.nextDouble();
+        System.out.println("Ingrese la tercera nota");
+        notas3 = entrada.nextDouble();
+        System.out.println("Ingrese la cuarta nota");
+        notas4 = entrada.nextDouble();
+
+        notaCualitativa = promedio(notas1, notas2, notas3, notas4);
+        mensaje = "El promedio de las notas " + notas1 + " , " + notas2
+                + " , " + notas3 + " , " + notas4 + " , es: "
+                + notaCualitativa;
+        String mensaje2 = mensajeMayus(mensaje);
+        System.out.printf("%s\n",mensaje2);
+    }
+
+    public static String promedio(double a, double b, double c, double d) {
+        double suma;
+        double promedio;
+        suma = a + b + c + d;
+        promedio = suma / 4;
+        String notaCualitativa = "";
+
+        if ((promedio >= 0) && (promedio <= 5)) {
+            notaCualitativa = "Regular";
+        } else {
+            if ((promedio >= 5.1) && (promedio <= 8)) {
+                notaCualitativa = "Bueno";
+            } else {
+                if ((promedio >= 8.1) && (promedio <= 9)) {
+                    notaCualitativa = "Muy bueno";
+                } else {
+                    if ((promedio >= 9.1) && (promedio <= 10)) {
+                        notaCualitativa = "Sobresaliente";
+                    }
+                }
+            }
+
+        }
+        return notaCualitativa;
     }
     
+    public static String mensajeMayus(String a){
+        String mensaje = a.toUpperCase();
+        return mensaje;
+    }
 }
--- ejercicios/Problemas/src/problemas/Problema4.java
+++ ejercicios/Problemas/src/problemas/Problema4.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -15,7 +17,67 @@ public class Problema4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        
+        String nombre; 
+        String cedula;
+        int opcion;
+        
+        System.out.println("Ingrese el nombre del cliente");
+        nombre = entrada.nextLine();
+        System.out.println("Ingree la cedula del cliente");
+        cedula = entrada.nextLine();
+        System.out.println("Ingrese el proceso que desea realizar");
+        opcion = entrada.nextInt();
+        
+        if(opcion == 1){
+            calcularValorLuz(nombre,cedula);
+        }else{
+            if(opcion == 2){
+                calcularValorPredio(nombre,cedula);
+            }
+        }
+        
     }
     
+    public static String calcularValorLuz(String a, String b){
+        Scanner entrada = new Scanner(System.in);
+        
+        double valorK;
+        double numeroK;
+        double total = 0;
+        String cadena = "";
+        
+        System.out.println("Ingrese el valor x kilovatio");
+        valorK = entrada.nextDouble();
+        System.out.println("Ingrese el numero de kilovatios usados");
+        numeroK = entrada.nextDouble();
+        
+        total = valorK * numeroK;
+        
+        cadena = "Cliente "+ a +" con cedula "+ b +" debe cancelar el valor de "
+                + total +"\n";
+        System.out.printf("%s\n",cadena);
+        return cadena;
+    }
+    
+    public static String calcularValorPredio(String a, String b){
+        Scanner entrada = new Scanner(System.in);
+        
+        double valorI;
+        double valorPredio = 0;
+        String cadena = "";
+        
+        System.out.println("Ingrese el valor del inmueble");
+        valorI = entrada.nextDouble();
+        
+        valorPredio = (valorI * 0.02);
+        
+        cadena = "Cliente "+ a +" con cedula "+ b +" tiene un bien inmueble"
+                + " valorado en "+ valorI +" y tiene que pagar de predio "+
+                valorPredio +"\n";
+        
+        System.out.printf("%s\n",cadena);
+        return cadena;
+    }
 }
