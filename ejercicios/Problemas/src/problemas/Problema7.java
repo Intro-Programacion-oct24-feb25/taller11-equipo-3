a52f1cd38a1faaaed0ec7fb892baed4fe6d0870f
Paulo Andres <pbustamante@utpl.edu.ec>
2025-01-09 21:46:23 -0500

    problema 7

--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -5,10 +5,43 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+    public static void main(String[] args) {
+        String[] ciudadesEcuador = elementosArreglo();
+        imprimirCiudad(ciudadesEcuador);
+    }
+
+    public static String[] elementosArreglo() {
+        Scanner entrada = new Scanner(System.in);
+
+        int numero;
+        System.out.println("Ingrese el numero de elementos");
+        numero = entrada.nextInt();
+        entrada.nextLine();
+
+        String[] ciudad = new String[numero];
+
+        for (int i = 0; i < numero; i++) {
+            System.out.println("Ingrese la ciudad");
+            ciudad[i] = entrada.nextLine();
+        }
+
+        return ciudad;
+    }
+
+    public static void imprimirCiudad(String[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if ((arreglo[i].length() == 4) || (arreglo[i].length() == 5)) {
+                System.out.printf("Ciudades con 4 o 5 caracteres: %s\n",
+                        arreglo[i]);
+            }
+        }
+    }
+
 }
