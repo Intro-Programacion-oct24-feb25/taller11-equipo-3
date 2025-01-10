8fcf39ac57a7fa199fbdae479e8ddcf7f096477b
Paulo Andres <pbustamante@utpl.edu.ec>
2025-01-09 20:29:29 -0500

    problemas 5 y 6

--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,5 +10,42 @@ package problemas;
  * @author reroes
  */
 public class Problema5 {
-    
+    public static void main(String[] args) {
+
+        int[][] arreglo1 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] arreglo2 = {{1, 2, 3}, {2, 2, 2}, {3, 1, 2}};
+        int[][] arreglo3 = new int[3][3];
+
+        for (int i = 0; i < arreglo1.length; i++) {
+            for (int j = 0; j < arreglo1[i].length; j++) {
+                arreglo3[i][j] = obtenerSuma(arreglo1[i][j],
+                        arreglo2[i][j]);
+            }
+        }
+
+        obtenerReporte(arreglo1);
+        obtenerReporte(arreglo2);
+        obtenerReporte(arreglo3);
+    }
+
+    public static void obtenerReporte(int[][] arreglo1) {
+        String cadena = "";
+        for (int i = 0; i < arreglo1.length; i++) {
+            for (int j = 0; j < arreglo1[i].length; j++) {
+                cadena = String.format("%s%d\t", cadena, arreglo1[i][j]);
+            }
+            cadena = String.format("%s\n", cadena);
+        }
+        System.out.println(cadena);
+    }
+
+    public static int obtenerSuma(int a, int b) {
+        int operacion;
+        operacion = a + b;
+        return operacion;
+
+    }
 }
+    
+    
+
